ef44f78847a88dac8a5eff45438f4e1328d8592d
Peter Demin <peterdemin@gmail.com>
2023-07-13 00:27:25 -0400

    Add markup conversion test

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -3,10 +3,17 @@ import shutil
 import tempfile
 
 from gdocsync.pandoc_client import PandocClient
-from .constants import CLEAN_HTML, CONVERTED_RST, IMAGES_DIR
+
+from .constants import (
+    CLEAN_HTML,
+    CONVERTED_RST,
+    IMAGES_DIR,
+    MARKUP_CLEAN_HTML,
+    MARKUP_CONVERTED_RST,
+)
 
 
-def test_html_bundle_to_rst_conversion():
+def test_html_bundle_with_image_to_rst_conversion():
     pandoc_client = PandocClient()
     with tempfile.TemporaryDirectory() as temp_dir:
         shutil.copytree(IMAGES_DIR, os.path.join(temp_dir, "images"))
@@ -14,6 +21,22 @@ def test_html_bundle_to_rst_conversion():
         pandoc_client.html_to_rst(CLEAN_HTML, result_path)
         with open(result_path, "rt", encoding="utf-8") as fobj:
             result = fobj.read()
+    # with open(CONVERTED_RST, "wt", encoding="utf-8") as fobj:
+    #     fobj.write(result)
     with open(CONVERTED_RST, "rt", encoding="utf-8") as fobj:
+        expected = fobj.read()
+    assert result == expected
+
+
+def test_html_bundle_with_markup_to_rst_conversion():
+    pandoc_client = PandocClient()
+    with tempfile.TemporaryDirectory() as temp_dir:
+        result_path = os.path.join(temp_dir, "output.rst")
+        pandoc_client.html_to_rst(MARKUP_CLEAN_HTML, result_path)
+        with open(result_path, "rt", encoding="utf-8") as fobj:
+            result = fobj.read()
+    # with open(MARKUP_CONVERTED_RST, "wt", encoding="utf-8") as fobj:
+    #     fobj.write(result)
+    with open(MARKUP_CONVERTED_RST, "rt", encoding="utf-8") as fobj:
         expected = fobj.read()
     assert result == expected
