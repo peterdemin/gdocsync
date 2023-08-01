eb322ca863a7358fae42d11d5f746b983212d9b2
Peter Demin <peterdemin@gmail.com>
2023-07-31 20:47:28 -0400

    Rerequest authentication on refresh failure

--- src/gdocsync/google_drive_client.py
+++ src/gdocsync/google_drive_client.py
@@ -3,6 +3,7 @@ import os.path
 from dataclasses import dataclass
 from typing import List
 
+from google.auth.exceptions import RefreshError
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -39,10 +40,16 @@ class GoogleAuth:
             else:
                 creds = None
             # If there are no (valid) credentials available, let the user log in.
+            fresh = False
             if not creds or not creds.valid:
                 if creds and creds.expired and creds.refresh_token:
-                    creds.refresh(Request())
-                else:
+                    try:
+                        creds.refresh(Request())
+                    except RefreshError:
+                        pass
+                    else:
+                        fresh = True
+                if not fresh:
                     flow = InstalledAppFlow.from_client_secrets_file(self._CREDENTIALS_PATH, SCOPES)
                     creds = flow.run_local_server(port=0)
                 # Save the credentials for the next run
